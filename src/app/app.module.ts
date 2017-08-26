25541b1c70286df07d9d9e826a69742d6e718288
Pavel <tretyakovpavel.ru@gmail.com>
2017-08-26 21:36:48 +0300

    Tasks components moved to the same route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { StoreModule } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { APP_BASE_HREF } from '@angular/common';
+
 import { AppComponent } from './app.component';
 import { tasksReducer } from './tasks/tasks.reducer';
 import { TasksModule } from './tasks/tasks.module';
+import { TasksRoutingModule } from './tasks/tasks-routing.module';
 
 @NgModule({
   imports: [
     FormsModule,
     TasksModule,
     BrowserModule,
+    TasksRoutingModule,
     StoreModule.forRoot({ tasks: tasksReducer }),
   ],
   declarations: [
